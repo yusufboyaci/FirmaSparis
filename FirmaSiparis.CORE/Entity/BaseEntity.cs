eb10e2923baad703d75bc10e9859c03b89af10fa
yusufboyaci <boyacyusuf@gamil.com>
2023-01-26 12:52:56 +0300

    Add project files.

---
+++ FirmaSiparis.CORE/Entity/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using FirmaSiparis.CORE.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirmaSiparis.CORE.Entity
+{
+    public abstract class BaseEntity : IEntity<Guid>
+    {
+        public BaseEntity()
+        {
+            Status = Status.None;
+            CreatedDate = DateTime.Now.ToUniversalTime();
+        }
+        public Guid Id { get; set; }
+        public Status Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ FirmaSiparis.CORE/Mapping/BaseEntityMap.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirmaSiparis.CORE.Mapping
+{
+    public class BaseEntityMap<T>
+    {
+    }
+}
